c2f4f0b10615426738f3950fa7d6af7d09937987
huangzijian <huangzijian@lamdaer.com>
2020-10-24 02:17:26 +0800

    feat(opengauss): init project

---
+++ components/info/index.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: Lamdaer
+ * @Date: 2020-10-24 01:54:38
+ * @LastEditTime: 2020-10-24 02:06:25
+ * @Description:
+ * @FilePath: /opengauss/components/info/index.js
+ */
+// components/info/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    jobList: {
+      type: Array,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+})
