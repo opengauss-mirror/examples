b04c67710e3d8ec0242f10ef63cb336642ba5db6
XudongXie <xiexudong23@otcaix.iscas.ac.cn>
2024-09-20 15:18:52 +0800

    revise

--- sqlTranslate/src/main/java/Main.java
+++ sqlTranslate/src/main/java/Main.java
@@ -8,17 +8,10 @@ import parser.OracleParser;
 public class Main {
     public static void main(String[] args) {
         String sql = "SELECT e.first_name, d.department_name FROM employees e JOIN departments d Using e.department_id = d.department_id;";
-//        String sql = "CASE WHEN column2 > 0 THEN 'Positive' ELSE 'Non-positive' END";
-//
         OracleLexer lexer = new OracleLexer(sql);
         lexer.printTokens();
         OracleParser parser = new OracleParser(lexer);
         ASTNode root = parser.parse();
-//        ASTNode root = OracleParser.parseException(lexer.getTokens());
-//        ASTNode root = OracleParser.parseLoop(lexer.getTokens());
-//        ASTNode root = OracleParser.parseIFELSE(lexer.getTokens());
-//        ASTNode root = OracleParser.parseCaseWhen(lexer.getTokens());
-//        ASTNode root = OracleParser.parseJoin(lexer.getTokens());
         System.out.println(root.toQueryString());
         System.out.println(root.getASTString());
         OpenGaussGenerator generator = new OpenGaussGenerator(root);
